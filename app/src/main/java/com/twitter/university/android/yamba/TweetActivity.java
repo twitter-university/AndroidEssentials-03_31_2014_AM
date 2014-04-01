8d8ea197a6af8e4c0602ead517be27d0161bdccb
G. Blake Meike <bmeike@twitter.com>
2014-04-01 10:09:43 -0700

    Landscape mode layout Using styles

--- app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
+++ app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
@@ -2,14 +2,46 @@ package com.twitter.university.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 
 public class TweetActivity extends Activity {
+    private static final String TAG = "TWEET";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: " + this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: " + this);
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: " + this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: " + this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: " + this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: " + this);
     }
 }
