0176f4d6f263d2b153c6d74056fbccebacdee452
G. Blake Meike <bmeike@twitter.com>
2014-04-02 09:55:22 -0700

    working AsyncTask

--- app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
+++ app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
@@ -1,19 +1,50 @@
 package com.twitter.university.android.yamba;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+// If mutable data is accessed from more than one thread
+// all access must be performed holding a single lock
 
 
 public class TweetActivity extends Activity {
     private static final String TAG = "TWEET";
+
+    static class Poster extends AsyncTask<String, Void, Integer> {
+        private Context ctxt;
+        Poster(Context ctxt) { this.ctxt = ctxt; }
+
+        // PRETEND TO SEND ON THE NETWORK
+
+        @Override
+        protected Integer doInBackground(String... strings) {
+            String tweet = strings[0];
+
+            try { Thread.sleep(1000 * 30); }
+            catch (InterruptedException e) { }
+
+            return Integer.valueOf(R.string.tweet_succeeded);
+        }
+
+        @Override
+        protected void onPostExecute(Integer status) {
+            poster = null;
+            Toast.makeText(ctxt, status.intValue(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    static Poster poster;
 
     private int okColor;
     private int warnColor;
@@ -45,6 +76,9 @@ public class TweetActivity extends Activity {
         countView = (TextView) findViewById(R.id.tweet_count);
 
         submitButton = (Button) findViewById(R.id.tweet_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) { post(); }
+        });
 
         tweetView = ((EditText) findViewById(R.id.tweet_tweet));
         tweetView.addTextChangedListener(
@@ -77,7 +111,20 @@ public class TweetActivity extends Activity {
         countView.setTextColor(color);
     }
 
+    void post() {
+        if (null != poster) { return; }
+
+        String tweet = tweetView.getText().toString();
+        if (!checkTweetLen(tweet.length())) { return; }
+
+        tweetView.setText("");
+        submitButton.setEnabled(false);
+
+        poster = new Poster(getApplicationContext());
+        poster.execute(tweet);
+    }
+
     private boolean checkTweetLen(int n) {
-        return (errMax < n) && (tweetLenMax > n);
+        return (errMax <= n) && (tweetLenMax > n);
     }
 }
